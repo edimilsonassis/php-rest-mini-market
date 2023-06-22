2bd9ea846d29b566c0479370f2511ec6565ab615
Edimilson <edy-assis@hotmail.com>
2023-06-22 12:19:50 -0300

    str_replace

--- backend/autoload.php
+++ backend/autoload.php
@@ -7,17 +7,20 @@ require_once 'vendor/autoload.php';
 date_default_timezone_set("America/Fortaleza");
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 
-const __ds = '/';
+const __ds = DIRECTORY_SEPARATOR;
 const __env = __DIR__ . __ds;
 
 $dotenv = new Dotenv();
 $dotenv->load(__env . '/.env');
 
 spl_autoload_register(function ($classe) {
-    $file = __env . $classe . '.php';
+    $classe = str_replace('\\', __ds, $classe);
+    $file   = __env . $classe . '.php';
 
-    // if (file_exists($file) && !is_dir($file))
+    if (file_exists($file) && !is_dir($file)) {
         require_once $file;
-    // else
-        // throw new \Exception('Error when opening class ' . $file, 1);
+    } else {
+        require_once __env . 'http/Exception.php';
+        throw new Exception('Error when opening class ' . $file, 1);
+    }
 });
